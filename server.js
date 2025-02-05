a51b0ce09b0f3769f6e2cf16ac6e5ee22df303fd
KamilKerimli <kamilmk-sp102@code.edu.az>
2025-02-05 22:11:47 +0400

    "server.js" file created and test API

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running!');
+});
+
+app.get('/api/message', (req, res) => {
+    res.json({ message: "Hello from Node.js API!" });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
